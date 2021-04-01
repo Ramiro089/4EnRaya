392b7bfe79e2a7bff9a2cad1ceed6fd64cbdf311
Ramiro G <ramirogatto089@gmail.com>
2021-03-31 21:58:49 -0300

    Commit V2(Posibilidad de tirar ficha de 1 a 7, y muestra ganador)

--- 4EnRaya.py
+++ 4EnRaya.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 def tableroVacio():
     return[
         [0, 0, 0, 0, 0, 0, 0],
@@ -8,13 +11,18 @@ def tableroVacio():
         [0, 0, 0, 0, 0, 0, 0],
     ]
 
-def SoltarFichaEnColumna(ficha, columna, tablero):
+def SoltarFichaEnColumna(ficha, columna, tablero, secuncia):
     for fila in range(6, 0, -1):
-        if tablero [fila - 1][columna - 1] == 0:
-            tablero [fila - 1][columna - 1 ] == ficha
+        if columna > 7:
+            os.system('cls')
+            print ("Ingrese un numero entre 1-6")
+            return 
+        elif tablero [fila - 1][columna - 1] == 0:
+            secuncia[len(secuencia):] = [columna]
+            #secuencia.append([columna])
+            os.system('cls')
             return
-
-
+    
 def completarTableroEnOrden(secuencia, tablero):
     n=0
     o=0
@@ -38,23 +46,133 @@ def completarTableroEnOrden(secuencia, tablero):
                     tablero [6-i-1][secuencia[n]-1] = 2
                     i=len(secuencia)
         n += 1
-    
+    return tablero
 
 def dibujarTablero(tablero):
     n=0
     while n<6:
      print (tablero[n][0],tablero[n][1],tablero[n][2],tablero[n][3],tablero[n][4],tablero[n][5],tablero[n][6])  
      n += 1
+    return tablero
 
-tablero = [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ]
+def ganador(tablero):
+    i=5
+    while i >= 0:
+        j=0
+        while j <= 3:
+            if tablero[i][j] != 0 and tablero[i][j] == tablero[i][j+1] and tablero[i][j] == tablero[i][j+2] and tablero[i][j] == tablero[i][j+3]:
+                print("Gano el Jugador " + str(tablero[i][j])) 
+                sys.exit()
+            else:
+                j += 1
+        i -= 1
+    i=0
+    while i <= 6:
+        j=5
+        while j >= 3:
+            if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i] and tablero[j][i] == tablero[j-2][i] and tablero[j][i] == tablero[j-3][i]:
+                print("Gano el Jugador " + str(tablero[j][i])) 
+                sys.exit()
+            else:
+                j -= 1
+        i += 1
+    diagonal(tablero)
 
+def diagonal(tablero):
+    r=0
+    j=0
+    while r <=6 :
+        if r==0:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 3:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i+1] and tablero[j][i] == tablero[j-2][i+2] and tablero[j][i] == tablero[j-3][i+3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        elif r==1:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 3:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i+1] and tablero[j][i] == tablero[j-2][i+2] and tablero[j][i] == tablero[j-3][i+3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        elif r==2:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 4:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i+1] and tablero[j][i] == tablero[j-2][i+2] and tablero[j][i] == tablero[j-3][i+3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        elif r==3:
+            i=r
+            j=5
+            if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i+1] and tablero[j][i] == tablero[j-2][i+2] and tablero[j][i] == tablero[j-3][i+3]:
+                print("Gano el Jugador " + str(tablero[j][i])) 
+                sys.exit()
+            elif tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i-1] and tablero[j][i] == tablero[j-2][i-2] and tablero[j][i] == tablero[j-3][i-3]:
+              print("Gano el Jugador " + str(tablero[j][i])) 
+              sys.exit()
+        elif r==4:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 4:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i-1] and tablero[j][i] == tablero[j-2][i-2] and tablero[j][i] == tablero[j-3][i-3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        elif r==5:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 3:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i-1] and tablero[j][i] == tablero[j-2][i-2] and tablero[j][i] == tablero[j-3][i-3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        elif r==6:
+            i=r
+            while i <= 6:
+                j=5
+                while j >= 3:
+                    if tablero[j][i] != 0 and tablero[j][i] == tablero[j-1][i-1] and tablero[j][i] == tablero[j-2][i-2] and tablero[j][i] == tablero[j-3][i-3]:
+                        print("Gano el Jugador " + str(tablero[j][i])) 
+                        sys.exit()
+                    else:
+                        j -= 1
+                i += 1
+        r += 1
+        
+
+tablero = tableroVacio()
 secuencia = [1, 2, 3, 1]
-completarTableroEnOrden(secuencia, tablero)
-dibujarTablero(tablero)
+c=0
+f=0
+os.system('cls')
+#dibujarTablero (tablero)
+while 1 > 0:
+    c = int(input("Ingrese columna: "))
+    if len(secuencia)%2 == 0:
+        f=1
+    else:
+        f=2
+    SoltarFichaEnColumna(f,c,tablero,secuencia)
+    tablero = dibujarTablero(completarTableroEnOrden(secuencia, tableroVacio()))
+    ganador(tablero)
+    
